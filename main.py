a940391a65a28137aab0952ed7e31588da1bcaf3
yasarsultan <yasersultan03@gmail.com>
2023-05-24 21:00:36 +0530

    ETL with API

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import pandas as pd
 import requests
+import pandas as pd
 
+def main():
+    # Calling api
+    url = "http://data.fixer.io/api/latest?access_key=7bc643487cdb57f1d92bdd4fa5c7ebdb"
+    text = requests.get(url)
+    # print(text.json())
+
+    dataframe = pd.DataFrame(text.json())
+    # print(dataframe)
+
+    dataframe.drop(columns=['success', 'timestamp', 'base', 'date'], inplace=True)
+    dataframe.rename(columns={"rates": "EUR_rates"}, inplace=True)
+    # print(dataframe)
+
+    dataframe.to_csv("exchangerates.csv")
+
+main()
